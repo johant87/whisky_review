882c1fccb4916a93bd8c9946b8a9d114d5057528
johant87 <jtillaart@hotmail.com>
2016-02-28 14:57:27 +0100

    controller json for whiskies and added puma server

--- app/controllers/whiskies_controller.rb
+++ app/controllers/whiskies_controller.rb
@@ -1,73 +1,56 @@
 class WhiskiesController < ApplicationController
-  before_action :set_whisky, only: [:show, :edit, :update, :destroy]
+    def index
+      render json: {
+        meta: {
+          count: Whisky.count,
+          page: 0
+        },
+        whiskies: Whisky.order(:name)
+      }
+    end
 
-  # GET /whiskies
-  # GET /whiskies.json
-  def index
-    @whiskies = Whisky.all
-  end
+    def show
+      whisky = Whisky.find(params[:id])
+      render json: { whisky: whisky }
+    end
 
-  # GET /whiskies/1
-  # GET /whiskies/1.json
-  def show
-  end
-
-  # GET /whiskies/new
-  def new
-    @whisky = Whisky.new
-  end
-
-  # GET /whiskies/1/edit
-  def edit
-  end
-
-  # POST /whiskies
-  # POST /whiskies.json
-  def create
-    @whisky = Whisky.new(whisky_params)
-
-    respond_to do |format|
-      if @whisky.save
-        format.html { redirect_to @whisky, notice: 'Whisky was successfully created.' }
-        format.json { render :show, status: :created, location: @whisky }
+    def create
+      if whisky = Whisky.create(whisky_params)
+        render json: { whisky: whisky }
       else
-        format.html { render :new }
-        format.json { render json: @whisky.errors, status: :unprocessable_entity }
+        render json: {
+          message: "Could not create whisky",
+          errors: whisky.errors,
+        }, status: :unprocessible_entity
       end
     end
-  end
 
-  # PATCH/PUT /whiskies/1
-  # PATCH/PUT /whiskies/1.json
-  def update
-    respond_to do |format|
-      if @whisky.update(whisky_params)
-        format.html { redirect_to @whisky, notice: 'Whisky was successfully updated.' }
-        format.json { render :show, status: :ok, location: @whisky }
+    def update
+      whisky = Whisky.find(params[:id])
+
+      if whisky.update(whisky_params)
+        render json: { whisky: whisky }
       else
-        format.html { render :edit }
-        format.json { render json: @whisky.errors, status: :unprocessable_entity }
+        render json: {
+          message: "Could not update whisky",
+          errors: whisky.errors,
+        }, status: :unprocessible_entity
       end
     end
-  end
 
-  # DELETE /whiskies/1
-  # DELETE /whiskies/1.json
-  def destroy
-    @whisky.destroy
-    respond_to do |format|
-      format.html { redirect_to whiskies_url, notice: 'Whisky was successfully destroyed.' }
-      format.json { head :no_content }
+    def destroy
+      whisky = Whisky.find(params[:id])
+
+      if whisky.destroy
+        render json: { whisky: nil }
+      else
+        render json: {
+          message: "Could not destroy whisky, please try again",
+        }, status: :unprocessible_entity
+      end
     end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_whisky
-      @whisky = Whisky.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def whisky_params
       params.require(:whisky).permit(:name, :description, :origin, :taste, :photo)
     end
